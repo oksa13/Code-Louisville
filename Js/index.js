d841c5727011321eed130fa9d612bca5829d6bbe
oksa13 <oksanakusyo@gmail.com>
2021-04-02 10:24:45 -0400

    update

--- Js/index.js
+++ Js/index.js
@@ -1,7 +1,6 @@
 
-//this will alert the user when the logo is clicked that the homepage will be created soon
-const myMessage = ('Out Adventuring! Home Webpage Coming Soon');
-const myMessageDays = ('Out Adventuring! This Journey Will Be Added Soon');
+const myMessage = ('Home Webpage Coming Soon');
+const myMessageDays = ('This Journey Will Be Added Soon');
 
 function myFunction(myMessage) {
     alert(myMessage);
