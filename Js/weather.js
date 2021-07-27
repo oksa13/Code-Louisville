fcabea187eba041fcbdac1c7843ce25ab79c9174
oksa13 <oksanakusyo@gmail.com>
2021-07-27 14:21:42 -0400

    added API weather

---
+++ Js/weather.js
@@ -0,0 +1,6 @@
+(function() {
+    var d = document, s = d.createElement('script');
+    s.src = 'https://www.visualcrossing.com/widgets/forecast-simple/weather-forecast-widget-simple.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+    })();
